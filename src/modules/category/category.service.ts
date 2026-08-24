7996e943346c15376d82039bc2886f7aaa3792b3
fh-ratul <ferdoushasan387@gmail.com>
2026-08-24 07:26:31 +0600

    fix vercel esm imports

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../../lib/prisma";
+import { prisma } from "../../lib/prisma.js";
 
 const getAllCategories = async () => {
   return prisma.category.findMany({ orderBy: { name: "asc" } });
